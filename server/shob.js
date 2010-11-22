d70ea0d7e9bf9d25e75290fa77f5fafb2e20dc88
Mathieu Lecarme <mathieu@garambrogne.net>
2010-11-23 00:22:48 +0100

    init url

--- server/shob.js
+++ server/shob.js
@@ -3,19 +3,7 @@ var url = require('url'),
 	xmpp = require('node-xmpp'),
 	util = require('util');
 
-function WebServer(port, ip) {
-	this.jids = {};
-	this.httpd = http.createServer(function (req, res) {
-		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.end('Hello World\n');
-	});
-	this.httpd.listen(port, ip);
-	console.log('Server running at http://' + ip + ':' + port);
-}
-
-exports.WebServer = WebServer;
-
-function Shob(jid, pwd, urlback) {
+function Shob(jid, pwd, urlback, cb) {
 	var u = url.parse(urlback);
 	var back = http.createClient(u.port, u.hostname);
 	var cl = new xmpp.Client({jid: jid + '/shob', password: pwd});
@@ -25,8 +13,8 @@ function Shob(jid, pwd, urlback) {
 		});
 	cl.on('online',
 		function() {
-			cl.send(new xmpp.Element('presence',{})
-			);
+			cl.send(new xmpp.Element('presence',{}));
+			cb.apply(this);
 		});
 	cl.on('stanza',
 		function(stanza) {
@@ -48,6 +36,43 @@ function Shob(jid, pwd, urlback) {
 		});
 }
 
+
+function WebServer(port, ip) {
+	var jids = {};
+	this.jids = jids;
+	this.httpd = http.createServer(function (req, res) {
+		var u = url.parse(req.url);
+		var data = '';
+		req.on('data', function(chunk) {
+			data += chunk;
+		});
+		req.on('end', function() {
+			console.log(data);
+			var jid = u.pathname.split('/')[1];
+			console.log(jids);
+			if(jids[jid] == null) {
+				res.writeHead(404, {'Content-Type': 'text/plain'});
+				res.end('not a user\n');
+			} else {
+				if(jids[jid].shob == null) {
+					jids[jid].shob = new Shob(jids[jid].jid, jids[jid].pwd, jids[jid].url, function() {
+						console.log(jids[jid].jid + ' is online');
+						res.writeHead(200, {'Content-Type': 'text/plain'});
+						res.end('');
+					});
+				} else {
+					res.writeHead(200, {'Content-Type': 'text/plain'});
+					res.end('Hello World\n');
+				}
+			}
+		});
+	});
+	this.httpd.listen(port, ip);
+	console.log('Server running at http://' + ip + ':' + port);
+}
+
+exports.WebServer = WebServer;
+
 WebServer.prototype.register = function(jid, pwd, url) {
-	this.jids[jid] = new Shob(jid, pwd, url);
+	this.jids[jid] = {jid: jid, pwd: pwd, url: url};
 };
